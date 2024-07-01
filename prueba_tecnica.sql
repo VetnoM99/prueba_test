b3ec9eac534c32dc89ea55dccb63b23de45a112c
Eric Alessi <eric.fabricio.alessi@gmail.com>
2024-07-01 09:52:40 +0200

    sql terminado

---
+++ prueba_tecnica.sql
@@ -0,0 +1,16 @@
+drop database if exists db_prueba_tecnica;
+create database if not exists db_prueba_tecnica;
+use db_prueba_tecnica;
+create table Articles(
+id int unsigned primary key not null auto_increment,
+titular char(90),
+contenido char(30)
+);
+
+INSERT INTO  Aricles (titular, contenido) VALUES 
+('María', 'Gol de Argenina'),
+('Juan', 'Mantener el entusiasmo'),
+('Ana', 'no puedo perder'),
+('Pedro', 'Herencia de los padres'),
+('Laura', 'Lo se no tiene sentido');
+
