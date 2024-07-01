165e98dd267dc17159f29879281d5b95a9c440b7
Eric Alessi <eric.fabricio.alessi@gmail.com>
2024-07-01 10:54:44 +0200

    js en forEach terminado

--- jsonFalsificado.js
+++ jsonFalsificado.js
@@ -15,3 +15,36 @@ const arrays = [
         { titular: "Laura", contenido: "Lo se no tiene sentido" },
     ]
 ];
+
+function displayArrays(arrays) {
+    const container = document.getElementById('mostrar');
+
+    arrays.forEach((array, index) => {
+        const arrayDiv = document.createElement('div');
+        arrayDiv.classList.add('array');
+
+        const title = document.createElement('h2');
+        title.textContent = `Array nº ${index + 1}`;
+        arrayDiv.appendChild(title);
+
+        array.forEach(item => {
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('item');
+
+            const itemTitle = document.createElement('h3');
+            itemTitle.textContent =`Titular: ${item.titular}`;
+            itemDiv.appendChild(itemTitle);
+
+            const itemContent = document.createElement('p');
+            itemContent.textContent = `Contenido: ${item.contenido}`;
+            itemDiv.appendChild(itemContent);
+
+            arrayDiv.appendChild(itemDiv);
+        });
+
+        container.appendChild(arrayDiv);
+    });
+}
+
+// Mostrar los arrays en la página
+displayArrays(arrays);
